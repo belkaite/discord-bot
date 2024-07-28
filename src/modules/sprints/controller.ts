75a9cb54dd3764c647a729903a2ad911a58ae0d3
Monika <monika.belkaite@gmail.com>
2024-07-28 19:48:54 +0300

    feat: create basic endpoints and test for sprint controller

--- src/modules/sprints/controller.ts
+++ src/modules/sprints/controller.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import type { Kysely } from 'kysely'
 import buildRepository from './repository'
 import type { DB } from '@/database/types'
+import * as schema from './schema'
 
 export default (database: Kysely<DB>) => {
   const router = Router()
@@ -11,6 +12,55 @@ export default (database: Kysely<DB>) => {
     try {
       const allSprints = await sprints.selectAll()
       res.status(200).json(allSprints)
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+  })
+
+  router.get('/:id', async (req, res) => {
+    try {
+      const id = schema.parseId(req.params.id)
+      const sprint = await sprints.selectById(id)
+      if (sprint) {
+        res.status(200).json(sprint)
+      } else {
+        res.status(404).json({ error: e.message })
+      }
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+  })
+
+  router.post('/', async (req, res) => {
+    try {
+      const body = schema.parseInsertable(req.body)
+      const sprint = await sprints.create(body)
+      res.status(201).json(sprint)
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+  })
+
+  router.patch('/:id', async (req, res) => {
+    try {
+      const id = schema.parseId(req.params.id)
+      const bodyPatch = schema.parseUpdateable(req.body)
+      const sprint = await sprints.update(id, bodyPatch)
+      res.status(200).json(sprint)
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+  })
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const id = schema.parseId(req.params.id)
+      const sprint = await sprints.delete(id)
+      if (sprint) {
+        res.status(204).send() // 204 No Content
+      } else {
+        res.status(404).json({ error: 'Sprint not found' })
+      }
     } catch (e) {
       res.status(500).json({ error: e.message })
     }
--- src/modules/sprints/tests/sprints.spec.ts
+++ src/modules/sprints/tests/sprints.spec.ts
@@ -1,6 +1,7 @@
 import supertest from 'supertest'
 import createTestDatabase from '@tests/utils/createTestDatabase'
 import type { Kysely } from 'kysely'
+import express from 'express'
 import { fakeSprint, sprintMatcher } from './utils'
 import buildRepository from '../repository'
 import createApp from '@/app'
@@ -37,5 +38,53 @@ describe('GET', () => {
     const { body } = await supertest(app).get('/sprints').expect(200)
 
     expect(body).toEqual([sprintMatcher({})])
+  })
+})
+
+describe('GET /:id', () => {
+  it('should return the sprint if it exists', async () => {
+    await repository.create(fakeSprint({ id: 10 }))
+
+    const { body } = await supertest(app).get('/sprints/10').expect(200)
+
+    expect(body).toEqual(sprintMatcher({ id: 10 }))
+  })
+})
+
+describe('POST', () => {
+  it('should return 201 and created sprint', async () => {
+    const { body } = await supertest(app)
+      .post('/sprints')
+      .send(fakeSprint({}))
+      .expect(201)
+
+    expect(body).toEqual(sprintMatcher({}))
+  })
+})
+
+describe('PATCH /:id', () => {
+  it('allows updates', async () => {
+    await repository.create(fakeSprint({ id: 10 }))
+
+    const { body } = await supertest(app)
+      .patch('/sprints/10')
+      .send({ title: 'Web dev full course' })
+      .expect(200)
+
+    expect(body).toEqual(
+      sprintMatcher({ id: 10, title: 'Web dev full course' })
+    )
+  })
+
+  describe('DELETE /:id', () => {
+    it('allows delete the sprint', async () => {
+      await repository.create(fakeSprint({ id: 10 }))
+
+      await supertest(app).delete('/sprints/10').expect(204)
+
+      const { body } = await supertest(app).get('/sprints/10').expect(404)
+
+      expect(body.error).toBe('Sprint not found')
+    })
   })
 })
