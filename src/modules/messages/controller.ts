042cd8a239bc9b26450b96f6bcff4e496b17f211
Monika <monika.belkaite@gmail.com>
2024-08-16 23:06:07 +0300

    feat: add gif to congrats message

--- src/modules/messages/controller.ts
+++ src/modules/messages/controller.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
 import type { Kysely } from 'kysely'
 import { StatusCodes } from 'http-status-codes'
+import fetch from 'node-fetch'
 import { sendMessage } from '@/discordClient'
 import type { DB } from '@/database/types'
 import buildSprintsRepository from '@/modules/sprints/repository'
 import buildTemplatesRepository from '@/modules/templates/repository'
 import buildUsersRepository from '@/modules/users/repository'
+
+const { GIPHY_API_KEY } = process.env
+
+if (!GIPHY_API_KEY) {
+  throw new Error('Provide GIPHY API KEY')
+}
 
 export default (database: Kysely<DB>) => {
   const router = Router()
@@ -62,8 +69,21 @@ export default (database: Kysely<DB>) => {
       .replace('{lastName}', user.lastName)
       .replace('{sprintTitle}', sprint.title)
 
+    let gifUrl
+
+    try {
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=congratulations&rating=g`
+      const response = await fetch(url)
+      const data = await response.json()
+      const index = Math.floor(Math.random() * data.data.length)
+      gifUrl = data.data[index].url
+    } catch (error) {
+      console.error('Error fetching GIF from Giphy API:', error)
+    }
+
     try {
       await sendMessage(congratsMessage)
+      await sendMessage(gifUrl)
       return res.status(200).send('Message sent successfully')
     } catch (error) {
       return res.status(500).send('Error sending message')
